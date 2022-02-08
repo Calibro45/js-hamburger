5e61c8c68296b49f4403d7eb3e753bf10a2f014b
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-02-08 16:43:53 +0100

    fix indent end

--- main.js
+++ main.js
@@ -6,26 +6,27 @@ console.log(hamburgerMenuIcon);
 
 // rilevare il click per far apparire il menu
 
+const HAMBURGER_MENU = document.querySelector(".hamburger-menu");
+
 hamburgerMenuIcon.addEventListener('click', function() {
-
-    const HAMBURGER_MENU = document.querySelector(".hamburger-menu");
-
-    HAMBURGER_MENU.classList.add("active");
     
+    HAMBURGER_MENU.classList.add("active");
+
     console.log('ora mi vedi', HAMBURGER_MENU);
     
-    // individuo elemento che chiude menu in html e lo salvo
-    
-    const closeMenuIcon = document.querySelector(".close");
-    //console.log(closeMenuIcon);
-    
-    // rilevo il click per chiudere menu
-    
-    closeMenuIcon.addEventListener('click', function() {
-        
-        HAMBURGER_MENU.classList.remove("active");
-
-        console.log('ora non mi vedi')
-        
-    }) 
 })
+
+// individuo elemento che chiude menu in html e lo salvo
+
+const closeMenuIcon = document.querySelector(".close");
+//console.log(closeMenuIcon);
+
+// rilevo il click per chiudere menu
+
+closeMenuIcon.addEventListener('click', function() {
+    
+    HAMBURGER_MENU.classList.remove("active");
+
+    console.log('ora non mi vedi')
+    
+}) 
